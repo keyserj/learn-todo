f8b980384b06587e6452ee4cb8002cb6e866ad4c
Joel Keyser <keyser.joel@gmail.com>
2022-07-27 00:20:48 -0500

    hotmann!

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
       </Head>
 
       <section className={utilStyles.headingMd}>
-        <p>Ayo this is flameo hotman</p>
+        <p>Ayo this is flameo hotmann</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
